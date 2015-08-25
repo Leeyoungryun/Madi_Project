b92ca6d2b9843947e90e6a19ae403ed20b138040
23and <44448kg@hanmail.netl>
2015-08-25 17:34:32 +0900

    수정

--- src/emotionExtractor/TendencyCheck.java
+++ src/emotionExtractor/TendencyCheck.java
@@ -2,7 +2,7 @@ package emotionExtractor;
 
 public class TendencyCheck {
 	public static String result(String tendency, String emotion){
-		String sad = "033 133 233 234 235 236 237 333 334 335 336 337";
+		String sad = "033 133 233 234 235 236 237 333 334 335 336 337 377";
 		String hate = "022 023 122 123 222 223 224 225 226 227";
  		if(tendency.equals("yes")){
  			System.out.println();
